4c8049992fbf4f72c0d421523d73d5db884113a9
Russell Corbett <russellcorbett11@gmail.com>
2023-08-13 23:18:57 -0230

    GitHub Workflow

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'mun-burgundy': '#923343',
+				'mun-grey': '#737577',
+				'munemotorsports-navy': '#20364d',
+				'munemotorsports-yellow': '#eabc2a',
+				'munemotorsports-red': '#6d2636',
+				'munemotorsports-grey': '#20364d',
+			}
+		},
 	},
 	plugins: [],
 }
